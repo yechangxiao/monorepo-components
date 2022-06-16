5afcd1273eb2a6af7bbaeaa747b460528ad3300f
yechangxiao <yechangxiaoycx@qq.com>
2022-06-17 00:40:36 +0800

    [test]: 添加jest和相关的配置文件

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    // 告诉jest处理'*.vue'文件
+    'vue'
+  ],
+  transform: {
+    // 用vue-jest处理 *.vue文件
+    '.*\\.(vue)$': 'vue-jest',
+    // 用babel-jest处理js
+    '.*\\.(js)$': 'babel-jest'
+  }
+}
