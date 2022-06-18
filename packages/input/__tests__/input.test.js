990d2723005233526d1e98deacc42a7e10f638f5
yechangxiao <yechangxiaoycx@qq.com>
2022-06-19 00:37:47 +0800

    [test]: 添加input的一些测试

--- packages/input/__tests__/input.test.js
+++ packages/input/__tests__/input.test.js
@@ -6,4 +6,33 @@ describe('rb-input', () => {
     const wrapper = mount(Input)
     expect(wrapper.html()).toContain('input type="text"')
   })
+
+  test('input-password', () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        type: 'password'
+      }
+    })
+    expect(wrapper.html()).toContain('input type="password"')
+  })
+
+  test('input-value', () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        type: 'password',
+        value: 'admin'
+      }
+    })
+    expect(wrapper.props('value')).toBe('admin')
+  })
+
+  test('input-snapshot', () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        type: 'text',
+        value: 'admin'
+      }
+    })
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
 })
